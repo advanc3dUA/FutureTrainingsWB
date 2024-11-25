4ba25ce102483764a51275edbe82860d7185626a
Yuriy Gudimov <fash1k@gmail.com>
2024-11-25 10:39:41 +0100

    initial commit

---
+++ P01-SwiftUIAsyncImage/P01-SwiftUIAsyncImage/P01_SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  P01_SwiftUIAsyncImageApp.swift
+//  P01-SwiftUIAsyncImage
+//
+//  Created by Yuriy Gudimov on 25.11.24.
+//
+
+import SwiftUI
+
+@main
+struct P01_SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
