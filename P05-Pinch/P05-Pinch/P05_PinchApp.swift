1bb1e1b14e7f0493eb36745e75a23ca4e5a9672e
Yuriy Gudimov <fash1k@gmail.com>
2024-11-27 10:15:37 +0100

    Start of P05-Pinch

---
+++ P05-Pinch/P05-Pinch/P05_PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  P05_PinchApp.swift
+//  P05-Pinch
+//
+//  Created by Yuriy Gudimov on 27.11.24.
+//
+
+import SwiftUI
+
+@main
+struct P05_PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ P05-Pinch/P05-Pinch/Screens/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  P05-Pinch
+//
+//  Created by Yuriy Gudimov on 27.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        //MARK: - PROPERTY
+        
+        //MARK: - FUNCTION
+        
+        //MARK: - CONTENT
+        
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
