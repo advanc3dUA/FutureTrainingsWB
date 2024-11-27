1185a36533e5037f0c06b8a15a27897d6d764038
Yuriy Gudimov <fash1k@gmail.com>
2024-11-27 08:19:10 +0100

    Start P03-Wishlist

---
+++ P03-Wishlist/P03-Wishlist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  P03-Wishlist
+//
+//  Created by Yuriy Gudimov on 27.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ P03-Wishlist/P03-Wishlist/P03_WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  P03_WishlistApp.swift
+//  P03-Wishlist
+//
+//  Created by Yuriy Gudimov on 27.11.24.
+//
+
+import SwiftUI
+
+@main
+struct P03_WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
