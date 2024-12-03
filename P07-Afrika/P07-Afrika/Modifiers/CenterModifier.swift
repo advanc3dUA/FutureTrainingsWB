3c5d834c33467cd803e0beb78efd1a4c83345779
Yuriy Gudimov <fash1k@gmail.com>
2024-12-03 15:42:33 +0100

    Finish P07-Afrika

---
+++ P07-Afrika/P07-Afrika/Modifiers/CenterModifier.swift
@@ -0,0 +1,8 @@
+//
+//  CenterModifier.swift
+//  P07-Afrika
+//
+//  Created by Yuriy Gudimov on 03.12.24.
+//
+
+import Foundation
---
+++ P07-Afrika/P07-Afrika/View/CreditsView.swift
@@ -0,0 +1,18 @@
+//
+//  CreditsView.swift
+//  P07-Afrika
+//
+//  Created by Yuriy Gudimov on 03.12.24.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreditsView()
+}
