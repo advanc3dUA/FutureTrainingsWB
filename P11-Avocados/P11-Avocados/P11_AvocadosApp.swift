c0c55945445d3e34effac290453fd0b5bfbac963
Yuriy Gudimov <fash1k@gmail.com>
2024-12-09 09:11:00 +0100

    Start P11-Avocados

---
+++ P11-Avocados/P11-Avocados/P11_AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  P11_AvocadosApp.swift
+//  P11-Avocados
+//
+//  Created by Yuriy Gudimov on 09.12.24.
+//
+
+import SwiftUI
+
+@main
+struct P11_AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
