812fe8f6b1ebc002266ad39032e58bf857bf84aa
Yuriy Gudimov <fash1k@gmail.com>
2025-01-30 15:07:04 +0100

    Add symbolEffects animations

--- P16-DC-App1/P16-DC-App1/ContentView.swift
+++ P16-DC-App1/P16-DC-App1/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isTapped = false
+    
     var body: some View {
         ZStack {
             Image(.image1)
@@ -53,8 +55,10 @@ struct ContentView: View {
                 HStack {
                     HStack {
                         Image(systemName: "ellipsis")
+                            .symbolEffect(.pulse)
                         Divider()
                         Image(systemName: "sparkle.magnifyingglass")
+                            .symbolEffect(.scale.up)
                         Divider()
                         Image(systemName: "face.smiling")
                     }
@@ -80,6 +84,28 @@ struct ContentView: View {
             .padding(20)
             .offset(y: 80)
             
+            HStack(spacing: 30) {
+                Image(systemName: "wand.and.rays")
+                    .frame(width: 44)
+                    .symbolEffect(.variableColor.iterative.reversing)
+                    .symbolEffect(.bounce, value: isTapped)
+                Image(systemName: isTapped ? "pause.fill" : "play.fill")
+                    .frame(width: 44)
+                    .contentTransition(.symbolEffect(.replace))
+                    .onTapGesture {
+                        isTapped.toggle()
+                    }
+                Image(systemName: "bell.and.waves.left.and.right.fill")
+                    .frame(width: 44)
+                    .symbolEffect(.bounce, options: .speed(3).repeat(3), value: isTapped)
+            }
+            .foregroundStyle(.primary, .white)
+            .font(.largeTitle)
+            .padding(20)
+            .background(.ultraThinMaterial)
+            .modifier(RoundedRectGradientOverlayModifer())
+            .clipShape(.rect(cornerRadius: 20))
+            .offset(y: -44)
         }
         .frame(maxWidth: 400)
         .padding(20)
