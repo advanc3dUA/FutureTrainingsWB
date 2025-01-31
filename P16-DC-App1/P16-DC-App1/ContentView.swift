3096736e8c0c7476b688a621e8c82a14c201577e
Yuriy Gudimov <fash1k@gmail.com>
2025-01-31 12:12:24 +0100

    Add phaseAnimation

--- P16-DC-App1/P16-DC-App1/ContentView.swift
+++ P16-DC-App1/P16-DC-App1/ContentView.swift
@@ -20,100 +20,34 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fill)
                 .frame(width: isTapped ? 393 : 360, height: isTapped ? 600 : 300)
                 .clipShape(.rect(cornerRadius: isTapped ? 0 : 20))
+                .modifier(RoundedRectGradientOverlayModifer())
                 .offset(y: isTapped ? -200 : 0)
+                .phaseAnimator([1, 2], trigger: isTapped, content: { content, phase in
+                    // in this case we using phase's numbers
+                    content.blur(radius: phase == 2 ? 200 : 0)
+                    }
+                )
+
             
-            VStack(alignment: .center) {
-                Text("modern architecture, an isometric tiny house, cute illustration, minimalist, vector art, night view")
-                    .font(.subheadline)
-                HStack(spacing: 8) {
-                    VStack(alignment: .leading) {
-                        Text("Size")
-                            .foregroundStyle(.secondary)
-                        Text("1024x1024")
-                    }
-                    .font(.subheadline)
-                    .fontWeight(.semibold)
-                    
-                    Divider()
-                    
-                    VStack(alignment: .leading) {
-                        Text("Type")
-                            .foregroundStyle(.secondary)
-                        Text("Upscale")
-                    }
-                    .font(.subheadline)
-                    .fontWeight(.semibold)
-                    
-                    Divider()
-                    
-                    VStack(alignment: .leading) {
-                        Text("Date")
-                            .foregroundStyle(.secondary)
-                        Text("Today 5:19")
-                    }
-                    .font(.subheadline)
-                    .fontWeight(.semibold)
-                }
-                .frame(height: 44)
-                
-                HStack {
-                    HStack {
-                        Image(systemName: "ellipsis")
-                            .symbolEffect(.pulse)
-                        Divider()
-                        Image(systemName: "sparkle.magnifyingglass")
-                            .symbolEffect(.scale.up, isActive: isActive)
-                        Divider()
-                        Image(systemName: "face.smiling")
-                            .symbolEffect(.appear, isActive: isActive)
-                    }
-                    .padding()
-                    .frame(height: 44)
-                    .modifier(UnevenRoundedRectGradientOverlayModifer())
-                    .offset(x: -20, y: 20)
-                    
-                    Spacer()
-                    
-                    Image(systemName: "square.and.arrow.down")
-                        .padding()
-                        .frame(height: 44)
-                        .modifier(UnevenInvertedRoundedRectOverlayModifier())
-                        .offset(x: 20, y: 20)
-                }
-                
-            }
+            content
             .padding(20)
             .background(.ultraThinMaterial)
             .modifier(RoundedRectGradientOverlayModifer())
             .clipShape(.rect(cornerRadius: 20))
             .padding(40)
             .offset(y: isTapped ? 220 : 80)
-            
-            HStack(spacing: 30) {
-                Image(systemName: "wand.and.rays")
-                    .frame(width: 44)
-                    .symbolEffect(.variableColor.iterative.reversing)
-                    .symbolEffect(.bounce, value: isTapped)
-                    .opacity(isTapped ? 1 : 0)
-                    .blur(radius: isTapped ? 0 : 20)
-                Image(systemName: isTapped ? "pause.fill" : "play.fill")
-                    .frame(width: 44)
-                    .contentTransition(.symbolEffect(.replace))
-                    .onTapGesture {
-                        withAnimation(.spring()) {
-                            isTapped.toggle()
-                        }
-                    }
-                Image(systemName: "bell.and.waves.left.and.right.fill")
-                    .frame(width: 44)
-                    .symbolEffect(.bounce, options: .speed(3).repeat(3), value: isTapped)
-                    .opacity(isTapped ? 1 : 0)
-                    .blur(radius: isTapped ? 0 : 20)
-                    .onReceive(timer) { value in
-                        time = value
-                        isActive.toggle()
-                    }
+            .phaseAnimator([1, 1.1, 1.2]) { content, phase in
+                // here I use exact raw values in sequence, to use later. Both variant are valid
+                content.scaleEffect(phase)
+            } animation: { phase in
+                switch phase {
+                case 1: .bouncy
+                case 1.1: .snappy
+                default: .bouncy
+                }
             }
+            
+            play
             .frame(width: isTapped ? 220 : 50)
             .foregroundStyle(.primary, .white)
             .font(.largeTitle)
@@ -125,6 +59,97 @@ struct ContentView: View {
         }
         .frame(maxWidth: 400)
         .dynamicTypeSize(.xSmall ... .xLarge)
+    }
+    
+    var content: some View {
+        VStack(alignment: .center) {
+            Text("modern architecture, an isometric tiny house, cute illustration, minimalist, vector art, night view")
+                .font(.subheadline)
+            HStack(spacing: 8) {
+                VStack(alignment: .leading) {
+                    Text("Size")
+                        .foregroundStyle(.secondary)
+                    Text("1024x1024")
+                }
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Type")
+                        .foregroundStyle(.secondary)
+                    Text("Upscale")
+                }
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Date")
+                        .foregroundStyle(.secondary)
+                    Text("Today 5:19")
+                }
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            }
+            .frame(height: 44)
+            
+            HStack {
+                HStack {
+                    Image(systemName: "ellipsis")
+                        .symbolEffect(.pulse)
+                    Divider()
+                    Image(systemName: "sparkle.magnifyingglass")
+                        .symbolEffect(.scale.up, isActive: isActive)
+                    Divider()
+                    Image(systemName: "face.smiling")
+                        .symbolEffect(.appear, isActive: isActive)
+                }
+                .padding()
+                .frame(height: 44)
+                .modifier(UnevenRoundedRectGradientOverlayModifer())
+                .offset(x: -20, y: 20)
+                
+                Spacer()
+                
+                Image(systemName: "square.and.arrow.down")
+                    .padding()
+                    .frame(height: 44)
+                    .modifier(UnevenInvertedRoundedRectOverlayModifier())
+                    .offset(x: 20, y: 20)
+            }
+            
+        }
+    }
+    
+    var play: some View {
+        HStack(spacing: 30) {
+            Image(systemName: "wand.and.rays")
+                .frame(width: 44)
+                .symbolEffect(.variableColor.iterative.reversing)
+                .symbolEffect(.bounce, value: isTapped)
+                .opacity(isTapped ? 1 : 0)
+                .blur(radius: isTapped ? 0 : 20)
+            Image(systemName: isTapped ? "pause.fill" : "play.fill")
+                .frame(width: 44)
+                .contentTransition(.symbolEffect(.replace))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        isTapped.toggle()
+                    }
+                }
+            Image(systemName: "bell.and.waves.left.and.right.fill")
+                .frame(width: 44)
+                .symbolEffect(.bounce, options: .speed(3).repeat(3), value: isTapped)
+                .opacity(isTapped ? 1 : 0)
+                .blur(radius: isTapped ? 0 : 20)
+                .onReceive(timer) { value in
+                    time = value
+                    isActive.toggle()
+                }
+        }
     }
 }
 
---
+++ P16-DC-App1/P16-DC-App1/EmptyModifier.swift
@@ -0,0 +1,14 @@
+//
+//  EmptyModifier.swift
+//  P16-DC-App1
+//
+//  Created by Yuriy Gudimov on 31.01.25.
+//
+
+import SwiftUI
+
+struct EmptyModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+    }
+}
